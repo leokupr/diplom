9d4e3240dd002c6fcfd36b34d5d01b0fe674e673
leokupr <leo.kupr@yandex.ru>
2024-05-08 00:01:07 +0300

    ok

---
+++ about.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Диплом</title>
+</head>
+<body>
+<?php require "blocks/header.php" ?>
+
+<div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">О проекте</h1>
+        <p class="lead text-body-secondary">Здесь вы можете ознакомиться с особенностями нашего портала!</p>
+      </div>
+</div>
